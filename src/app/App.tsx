a04cb6fcf5886eec6167487e10f9f77c570ef771
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-22 16:03:57 +0545

    add AppLinks to wrap images

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,12 +13,9 @@ function App() {
         <AppLink to="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </AppLink>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a> */}
-        <a href="https://react.dev" target="_blank">
+        <AppLink to="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </AppLink>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
