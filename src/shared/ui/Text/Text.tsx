f712a108a219b816efd07b19dd7324eb8bea2649
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-24 18:48:17 +0545

    add a Text base template

---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,42 @@
+import { memo } from 'react';
+import cls from './Text.module.scss'
+import addClasses from '../../lib/addClasses';
+
+export type TextVariant = 'primary' | 'error' | 'accent';
+
+export type TextAlign = 'right' | 'left' | 'center';
+
+export type TextSize = 's' | 'm' | 'l';
+
+interface TextProps {
+  className?: string;
+  title?: string;
+  text?: string;
+  bold?: boolean;
+  variant?: TextVariant;
+  align?: TextAlign;
+  size?: TextSize;
+}
+type HeaderTagType = 'h1' | 'h2' | 'h3';
+
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+  s: 'h3',
+  m: 'h2',
+  l: 'h1',
+};
+
+const mapSizeToClass: Record<TextSize, string> = {
+  s: cls.size_s,
+  m: cls.size_m,
+  l: cls.size_l,
+};
+
+
+export const Test = memo((props: TextProps) => {
+  const { className, variant = "primary", align = "left", size = "m", title, text, bold } = props;
+
+  const HeaderTag = mapSizeToHeaderTag[size];
+  const sizeClass = mapSizeToClass[size];
+  const cardClasses = [className, cls[variant], cls[align], sizeClass];
+  return <p className={addClasses(cls.Text, {}, cardClasses)}></p>
+})
