474d025b8091d6ac967dc9b94ce00ef9ce9d289f
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-27 08:48:14 +0545

    update App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -28,9 +28,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Text className="read-the-docs" text="Click on the Vite and React logos to learn more" />
     </>
   )
