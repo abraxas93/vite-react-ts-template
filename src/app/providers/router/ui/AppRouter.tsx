ce3e73504b3b1597ad353faea6dbbf8ea5a21d8a
Savchuk Alexandr <savchyk.a@gmail.com>
2023-10-07 22:13:49 +0530

    add a Suspense wrap

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom"
-import MainPage from "../../../../pages/MainPage/ui/MainPage"
+import { MainPage } from "../../../../pages/MainPage"
 import ProfilePage from "../../../../pages/ProfilePage"
-import { memo } from "react"
+import { Suspense, memo } from "react"
 
 
 
 export const AppRouter = memo(() => {
   return (
-    <Routes>
-      <Route path='/' element={<MainPage />} />
-      <Route path='/profile' element={<ProfilePage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading....</div>}>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </Suspense>
   )
 })
 
